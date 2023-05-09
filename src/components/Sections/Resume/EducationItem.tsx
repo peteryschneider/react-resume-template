7e79f200f7e0231a84322de70b775f00c99e5599
peteryschneider <peteryschneider@gmail.com>
2023-05-09 10:50:52 -0700

    education formatting

--- src/components/Sections/Resume/EducationItem.tsx
+++ src/components/Sections/Resume/EducationItem.tsx
@@ -13,47 +13,23 @@ const EducationItem: FC<{item: EducationItem}> = memo(({item}) => {
 
   return (
     <div className="flex flex-col pb-8 text-center last:pb-0 md:text-left">
-      <div className="flex flex-col">
-        <div className="flex items-start">
-          <div className="flex flex-col">
-            <h2 className="text-l font-bold">
-              <a
-                className="hover:text-blue-400"
-                href={link}
-                rel="noopener noreferrer"
-                target="_blank"
-              >
-                {school}
-              </a>
-            </h2>
-            <div className="flex items-center justify-center gap-x-2 md:justify-start">
-              <span className="flex-1 text-med font-medium sm:flex-none">
-                {degree}
-              </span>
-              <span>•</span>
-              <span className="flex-1 text-sm sm:flex-none">{date}</span>
-            </div>
-            <div className="flex items-center justify-center gap-x-2 md:justify-start">
-              <span className="flex-1 text-med font-medium sm:flex-none">
-                {specialization}
-              </span>
-            </div>
-          </div>
+      <div className="flex flex-col pb-0">
+        <h2 className="text-l font-bold">
+            <a className="hover:text-blue-400" href={link} rel="noopener noreferrer" target="_blank">{school}</a>
+        </h2>
+        <div className="flex items-center justify-center gap-x-2 md:justify-start">
+          <span className="flex-1 text-med font-medium sm:flex-none">{degree}</span>
+          <span>•</span>
+          <span className="flex-1 text-sm sm:flex-none">{date}</span>
         </div>
       </div>
+      <span className="flex-1 text-med font-medium sm:flex-none">{specialization}</span>
       {React.Children.count(content) > 0 && (
         <div className="pb-4">
           <div className="flex">
             {content !== '' && (
-              <button
-                className="ml-2 mr-4 focus:outline-none"
-                onClick={toggleContentVisibility}
-              >
-                {isContentVisible ? (
-                  <ChevronUpIcon className="w-5 h-5" />
-                ) : (
-                  <ChevronDownIcon className="w-5 h-5" />
-                )}
+              <button className="ml-2 mr-4 focus:outline-none" onClick={toggleContentVisibility}>
+                {isContentVisible ? (<ChevronUpIcon className="w-5 h-5" />) : (<ChevronDownIcon className="w-5 h-5" />)}
               </button>
             )}
           </div>
